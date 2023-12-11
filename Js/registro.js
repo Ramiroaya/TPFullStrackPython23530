3972cc44ea9888178d31ae856b166f4b0041b6f6
Ramiro Quiñoneros <quinionerosramiro@gmail.com>
2023-12-10 21:25:08 -0300

    correciones de codigo, para funcionamiento en pythonanywhere

--- Js/registro.js
+++ Js/registro.js
@@ -1,39 +1,60 @@
-const URL = "http://127.0.0.1:5000";
+const URL = "https://ayacodoacodo.pythonanywhere.com";
 
-document.getElementById('form').addEventListener('submit', async function (event) {
+document.getElementById('registration-form').addEventListener('submit', async function (event) {
   event.preventDefault();
+  var contrasena = document.getElementById('contrasena').value;
+  var confirmarContrasena = document.getElementById('confirmarContrasena').value;
+
+  if (contrasena !== confirmarContrasena) {
+      alert('Las contraseñas no coinciden.');
+      return;
+  }
 
   const formData = new FormData();
-  formData.append('usuario', document.getElementById('usuario').value);
-  formData.append('correo', document.getElementById('correo').value);
+  formData.append('nombre', document.getElementById('userName').value);
   formData.append('ciudad', document.getElementById('ciudad').value);
-  formData.append('contrasena', document.getElementById('contrasena').value);
-  formData.append('confirmarContrasena', document.getElementById('confirmarContrasena').value);
+  formData.append('email', document.getElementById('email').value);
+  formData.append('contrasena', document.getElementById('password').value);
+  formData.append('confirmarContrasena', document.getElementById('confirmPassword').value);
+  console.log(nombre);
 
-  //Solicitud POST al servidor
-  try {
-    const response = await fetch(URL + '/usuario', {
-      method: 'POST',
-      body: formData,
-  });
+   // Realizamos la solicitud POST al servidor
+   fetch(URL + 'usuario', {
+    method: 'POST',
+    body: formData // Aquí enviamos formData en lugar de JSON
+})
+    //Después de realizar la solicitud POST, se utiliza el método then() para manejar la respuesta del servidor.
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            // Si hubo un error, lanzar explícitamente una excepción
+            // para ser "catcheada" más adelante
+            throw new Error('Error al agregar el Usuario.');
+        }
+    })
 
-    if (response.ok) {
-      const data =  await response.json();
-      alert('Usuario creado:' + data.mensaje);
-    } else { //error, lanza una excepción para ser "catcheada" luego
-      throw new Error('Error en el registro. El usuario ya existe.');
-    }
-  } catch(error) {
-    alert(error.message);
-    console.error('Error:', error);
-  } finally {
-      document.getElementById('usuario').value = "";
-      document.getElementById('correo').value = "";
-      document.getElementById('ciudad').value = "";
-      document.getElementById('contrasena').value = "";
-      document.getElementById('confirmarContrasena').value = "";
-    }
-  });
+
+    // Respuesta OK
+    .then(function () {
+        // En caso de éxito
+        alert('Usuario agregado correctamente.');
+    })
+    .catch(function (error) {
+        // En caso de error
+        alert('Error al agregar el usuario.');
+        console.error('Error:', error);
+    })
+   .finally(function () {
+    // Restablece los valores del formulario
+    document.getElementById('userName').value = "";
+    document.getElementById('ciudad').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('password').value = "";
+    document.getElementById('confirmPassword').value = "";
+  })
+});
+
 
 /*--------------*/
 
